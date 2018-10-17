c8a713a1e77f5d06ba5dffc3de70ba6c692685c1
Max <s312@tut.by>
2018-10-17 21:46:30 +0300

    aaa

--- src/main/java/com/HomeController.java
+++ src/main/java/com/HomeController.java
@@ -1,15 +1,34 @@
 package com;
 
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public @ResponseBody String greeting() {
+    @RequestMapping(value = "/force")
+    @ResponseBody
+    public String greeting() throws ClientException, ApiException, IOException, URISyntaxException, InterruptedException
+    {
+        Main m = new Main();
+        m.force();
         return "Hello World";
+
+    }
+    @RequestMapping(value = "/")
+    @ResponseBody
+    public String greetings() {
+        return "Hello World 2";
+
     }
 
 }
--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -17,6 +17,7 @@ import org.apache.commons.io.FileUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -39,6 +40,7 @@ import java.util.Random;
 @Component
 public class Main
 {
+    private int idGroup = 2;
     public PDDocument doc;
     public String name;
     public Random r;
@@ -66,9 +68,16 @@ public class Main
         doc = PDDocument.load(ans);
     }
 
+
+    public Main bb()
+    {
+        return this;
+    }
+
     @PostConstruct
     public void main() throws IOException, ClientException, ApiException
     {
+
         System.out.println("PostConstruct");
         r = new Random();
         tc = HttpTransportClient.getInstance();
@@ -76,7 +85,7 @@ public class Main
         groupActor = new GroupActor(172656437,"e1682d74ee13b3c7e64cc9e9dad8ae15aa2c2785ad47a0cae7f3c2084b571c3f6afd124a452143b00d378");
         SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
         vk.messages().send(groupActor, 13466081).randomId(r.nextInt()).message("Start succesfull!! " + df.format(new Date())).execute();
-        vk.messages().send(groupActor).message("Bot start succesfull!! " + df.format(new Date())).randomId(r.nextInt()).chatId(2).execute();
+        vk.messages().send(groupActor).message("Bot start succesfull!! " + df.format(new Date())).randomId(r.nextInt()).chatId(idGroup).execute();
 
         URL siteURL1 = null;
         siteURL1 = new URL("http://www.mrk-bsuir.by/ru");
@@ -95,7 +104,53 @@ public class Main
         bf1.close();
     }
 
-    @Scheduled(initialDelay = 1000, fixedDelay = 100000)
+    public void force() throws IOException, ClientException, ApiException
+    {
+        r = new Random();
+        tc = HttpTransportClient.getInstance();
+        vk = new VkApiClient(tc);
+        groupActor = new GroupActor(172656437,"e1682d74ee13b3c7e64cc9e9dad8ae15aa2c2785ad47a0cae7f3c2084b571c3f6afd124a452143b00d378");
+        URL siteURL = null;
+        siteURL = new URL("http://www.mrk-bsuir.by/ru");
+        BufferedReader bf = new BufferedReader(new InputStreamReader(siteURL.openStream()));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while((line = bf.readLine()) != null)
+        {
+            sb.append(line);
+        }
+        bf.close();
+        int pos = sb.indexOf("Объявления");
+        int end = sb.indexOf(".pdf", pos);
+        String urlPDF = sb.substring(pos+76, end+4);
+        name = urlPDF;
+        URL url = new URL(urlPDF);
+        System.out.println(url);
+
+        updatePDF(url);
+
+        PDFRenderer pdfRenderer = new PDFRenderer(doc);
+        BufferedImage im = pdfRenderer.renderImageWithDPI(4, 200, ImageType.RGB);
+        im =im.getSubimage(105, 53, 1519, 698);
+        File out = new File("file.jpg");
+        ImageIO.write(im, "jpg",out);
+
+        doc.close();
+
+        PhotoUpload serverResponse = vk.photos().getMessagesUploadServer(groupActor).execute();
+        MessageUploadResponse messageUploadResponse = vk.upload().photoMessage(serverResponse.getUploadUrl(),out).execute();
+        List<Photo> photoList = vk.photos().saveMessagesPhoto(groupActor, messageUploadResponse.getPhoto())//.saveWallPhoto(groupActor, uploadResponse.getPhoto())
+                .server(messageUploadResponse.getServer())
+                .hash(messageUploadResponse.getHash())
+                .execute();
+
+        Photo photo = photoList.get(0);
+        String attachId = "photo" + photo.getOwnerId() + "_" + photo.getId();
+        vk.messages().send(groupActor).attachment(attachId).message("Force update").randomId(r.nextInt()).chatId(idGroup).execute();
+
+    }
+
+    @Scheduled(initialDelay = 1000, fixedDelay = 600000)
     public void loop() throws ClientException, ApiException, IOException, URISyntaxException, InterruptedException
     {
         System.out.println("Scheduled");
@@ -138,11 +193,7 @@ public class Main
 
             Photo photo = photoList.get(0);
             String attachId = "photo" + photo.getOwnerId() + "_" + photo.getId();
-            vk.messages().send(groupActor).attachment(attachId).randomId(r.nextInt()).chatId(2).execute();
-            //Привет для Коли vk.messages().send(groupActor, 152839977).attachment(attachId).message("ТЫ БЫЛ НЕ ПРАВ").randomId(r.nextInt()).execute();
-
-
-
+            vk.messages().send(groupActor).attachment(attachId).randomId(r.nextInt()).chatId(idGroup).execute();
 
         }
 
