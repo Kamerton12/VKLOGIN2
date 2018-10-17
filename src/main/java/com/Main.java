28ecf1ca763533929560fd813346e4059a2149d1
Max <s312@tut.by>
2018-10-17 20:28:55 +0300

    TEST

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -17,7 +17,10 @@ import org.apache.commons.io.FileUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.imageio.ImageIO;
 import javax.imageio.stream.FileImageOutputStream;
 import java.awt.image.BufferedImage;
@@ -26,15 +29,24 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+import java.util.Date;
 import java.util.Random;
 
+@Component
 public class Main
 {
-    public static PDDocument doc;
+    public PDDocument doc;
+    public String name;
+    public Random r;
+    public TransportClient tc;
+    public VkApiClient vk;
+    public GroupActor groupActor;
 
-    private static void updatePDF(URL url) throws IOException
+    private void updatePDF(URL url) throws IOException
     {
         InputStream is = url.openStream();
         int len = 0;
@@ -54,15 +66,16 @@ public class Main
         doc = PDDocument.load(ans);
     }
 
-
-    public static void main(String[] args) throws ClientException, ApiException, IOException, URISyntaxException, InterruptedException
+    @PostConstruct
+    public void main() throws IOException, ClientException, ApiException
     {
-
-        Random r = new Random();
-        TransportClient tc = HttpTransportClient.getInstance();
-        VkApiClient vk = new VkApiClient(tc);
-        GroupActor groupActor = new GroupActor(172656437,"e1682d74ee13b3c7e64cc9e9dad8ae15aa2c2785ad47a0cae7f3c2084b571c3f6afd124a452143b00d378");
-        vk.messages().send(groupActor, 13466081).randomId(r.nextInt()).message("Start succesfull!!").execute();
+        System.out.println("PostConstruct");
+        r = new Random();
+        tc = HttpTransportClient.getInstance();
+        vk = new VkApiClient(tc);
+        groupActor = new GroupActor(172656437,"e1682d74ee13b3c7e64cc9e9dad8ae15aa2c2785ad47a0cae7f3c2084b571c3f6afd124a452143b00d378");
+        SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        vk.messages().send(groupActor, 13466081).randomId(r.nextInt()).message("Start succesfull!! " + df.format(new Date())).execute();
 
         URL siteURL1 = null;
         siteURL1 = new URL("http://www.mrk-bsuir.by/ru");
@@ -77,53 +90,57 @@ public class Main
         int pos1 = sb1.indexOf("Объявления");
         int end1 = sb1.indexOf(".pdf", pos1);
         String urlPDF1 = sb1.substring(pos1+76, end1+4);
-        String name = urlPDF1;
+        name = urlPDF1;
         bf1.close();
-        while(true)
+    }
+
+    @Scheduled(initialDelay = 1000, fixedDelay = 100000)
+    public void loop() throws ClientException, ApiException, IOException, URISyntaxException, InterruptedException
+    {
+        System.out.println("Scheduled");
+        URL siteURL = null;
+        siteURL = new URL("http://www.mrk-bsuir.by/ru");
+        BufferedReader bf = new BufferedReader(new InputStreamReader(siteURL.openStream()));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while((line = bf.readLine()) != null)
         {
-            Thread.sleep(600000);
-            URL siteURL = null;
-            siteURL = new URL("http://www.mrk-bsuir.by/ru");
-            BufferedReader bf = new BufferedReader(new InputStreamReader(siteURL.openStream()));
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            while((line = bf.readLine()) != null)
-            {
-                sb.append(line);
-            }
-            bf.close();
-            int pos = sb.indexOf("Объявления");
-            int end = sb.indexOf(".pdf", pos);
-            String urlPDF = sb.substring(pos+76, end+4);
-            if(name != urlPDF)
-            {
-                name = urlPDF;
-                URL url = new URL(urlPDF);
-                System.out.println(url);
+            sb.append(line);
+        }
+        bf.close();
+        int pos = sb.indexOf("Объявления");
+        int end = sb.indexOf(".pdf", pos);
+        String urlPDF = sb.substring(pos+76, end+4);
+        if(!name.equals(urlPDF))
+        {
+            //System.out.println(name.equals(urlPDF));
+            name = urlPDF;
+            URL url = new URL(urlPDF);
+            System.out.println(url);
 
-                updatePDF(url);
+            updatePDF(url);
 
-                PDFRenderer pdfRenderer = new PDFRenderer(doc);
-                BufferedImage im = pdfRenderer.renderImageWithDPI(4, 200, ImageType.RGB);
-                im =im.getSubimage(105, 53, 1519, 698);
-                File out = new File("file.jpg");
-                ImageIO.write(im, "jpg",out);
+            PDFRenderer pdfRenderer = new PDFRenderer(doc);
+            BufferedImage im = pdfRenderer.renderImageWithDPI(4, 200, ImageType.RGB);
+            im =im.getSubimage(105, 53, 1519, 698);
+            File out = new File("file.jpg");
+            ImageIO.write(im, "jpg",out);
 
-                doc.close();
+            doc.close();
 
-                PhotoUpload serverResponse = vk.photos().getMessagesUploadServer(groupActor).execute();
-                MessageUploadResponse messageUploadResponse = vk.upload().photoMessage(serverResponse.getUploadUrl(),out).execute();
-                List<Photo> photoList = vk.photos().saveMessagesPhoto(groupActor, messageUploadResponse.getPhoto())//.saveWallPhoto(groupActor, uploadResponse.getPhoto())
-                        .server(messageUploadResponse.getServer())
-                        .hash(messageUploadResponse.getHash())
-                        .execute();
+            PhotoUpload serverResponse = vk.photos().getMessagesUploadServer(groupActor).execute();
+            MessageUploadResponse messageUploadResponse = vk.upload().photoMessage(serverResponse.getUploadUrl(),out).execute();
+            List<Photo> photoList = vk.photos().saveMessagesPhoto(groupActor, messageUploadResponse.getPhoto())//.saveWallPhoto(groupActor, uploadResponse.getPhoto())
+                    .server(messageUploadResponse.getServer())
+                    .hash(messageUploadResponse.getHash())
+                    .execute();
 
-                Photo photo = photoList.get(0);
-                String attachId = "photo" + photo.getOwnerId() + "_" + photo.getId();
-                vk.messages().send(groupActor).attachment(attachId).randomId(r.nextInt()).chatId(1).execute();
-                //Привет для Коли vk.messages().send(groupActor, 152839977).attachment(attachId).message("ТЫ БЫЛ НЕ ПРАВ").randomId(r.nextInt()).execute();
+            Photo photo = photoList.get(0);
+            String attachId = "photo" + photo.getOwnerId() + "_" + photo.getId();
+            vk.messages().send(groupActor).attachment(attachId).randomId(r.nextInt()).chatId(1).execute();
+            //Привет для Коли vk.messages().send(groupActor, 152839977).attachment(attachId).message("ТЫ БЫЛ НЕ ПРАВ").randomId(r.nextInt()).execute();
 
-            }
+
 
 
         }
